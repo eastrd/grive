3d11778641381d058b65920f5d1224ac212d1ead
eastrd <yjia285@student.monash.edu>
2019-03-10 15:25:04 +1100

    Refactor const naming to be isolated

---
+++ config.go
@@ -0,0 +1,10 @@
+package main
+
+// ACCDIR : Accounts directory
+const ACCDIR = "accounts/"
+
+// ACCCONFIG : Configuration file for all accounts
+const ACCCONFIG = ACCDIR + "accounts.txt"
+
+// STORAGEDIR : Uploaded file directory
+const STORAGEDIR = "storage/"
